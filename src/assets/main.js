60562336f6152a7d1610420de16199e01d10a889
IRHM <37304121+IRHM@users.noreply.github.com>
2022-10-28 17:34:37 +0100

    Disable slider on mobile and instead show as list

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,4 +9,60 @@ const sliderCfg = {
   }
 };
 
-new BlazeSlider(sliderEl, sliderCfg);
+let loadedSlider = false;
+let blazeSlider;
+
+function mobileOp() {
+  sliderEl.classList.add("mobile");
+  if (blazeSlider) {
+    // blazeSlider.prev(99);
+
+    setTimeout(() => {
+      // No way to disable slider, using this as quick fix.
+      // Drag event doesn't do anything when in transition.
+      blazeSlider.isTransitioning = true;
+    }, 300);
+  }
+}
+
+function desktopOp() {
+  sliderEl.classList.remove("mobile");
+
+  if (!loadedSlider) {
+    loadedSlider = true;
+    const head = document.getElementsByTagName("head")[0];
+
+    // Load slider css
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "https://unpkg.com/blaze-slider@1.8.0/dist/blaze.css";
+    head.appendChild(link);
+
+    // Load slider js
+    const script = document.createElement("script");
+    script.src = "https://unpkg.com/blaze-slider@1.8.0/dist/blaze-slider.min.js";
+    script.addEventListener("load", () => {
+      blazeSlider = new BlazeSlider(sliderEl, sliderCfg);
+      blazeSlider.isTransitioning = false;
+    });
+    script.addEventListener("error", (ev) => {
+      // try again?
+      console.error("Error loading slider js:", ev);
+    });
+    head.appendChild(script);
+  } else if (blazeSlider) {
+    blazeSlider.isTransitioning = false;
+  }
+}
+
+function opOnResize() {
+  if (window.innerWidth < 650) {
+    mobileOp();
+  } else {
+    desktopOp();
+  }
+}
+
+window.addEventListener("resize", opOnResize);
+opOnResize();
